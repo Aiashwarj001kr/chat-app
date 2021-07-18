8cd0c9cb58bb2b83b34d0b6d618629ef994f0b44
Aiashwarj001kr <aiashwarjsurin@gmail.com>
2021-07-18 17:23:53 +0530

    add firebase to project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyBPYZXagR0SJHwfKFvY53uHgsY9TYbKaMA",
+    authDomain: "chat-app-bb07d.firebaseapp.com",
+    projectId: "chat-app-bb07d",
+    storageBucket: "chat-app-bb07d.appspot.com",
+    messagingSenderId: "33536748886",
+    appId: "1:33536748886:web:503eb195559e68f2293af7"
+};
+
+firebase.initializeApp(config);
+
